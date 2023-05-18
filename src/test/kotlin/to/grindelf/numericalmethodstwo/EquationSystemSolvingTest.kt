8d6ac8281cda2399607a47f797cb655d6ec8f842
GrindelfP <grindelf.perlomutrovij@gmail.com>
2023-05-18 19:19:11 +0300

    add multiple solutions

--- src/test/kotlin/to/grindelf/numericalmethodstwo/EquationSystemSolvingTest.kt
+++ src/test/kotlin/to/grindelf/numericalmethodstwo/EquationSystemSolvingTest.kt
@@ -6,26 +6,29 @@ import to.grindelf.numericalmethodstwo.NewtonsMethod.solution
 
 class EquationSystemSolvingTest {
 
-    private val xStart = -0.8
-    private val yStart = -0.4
-    private val xResultInterval = listOf(-1.012549, -1.012540)
-    private val yResultInterval = listOf(-0.524649, -0.524640)
+    private val xesStart = listOf(-0.8, 1.0)
+    private val ysStart = listOf(-0.4, 0.5)
+    private val xesResultIntervals = listOf(listOf(-1.012559, -1.012530), listOf(1.321030, 1.321059))
+    private val ysResultInterval = listOf(listOf(-0.524659, -0.524630), listOf(0.997570, 0.997599))
 
     @Test
-    fun `GIVEN functions WHEN Newtons numeric method is applied THEN solution is returned`() {
-        val solution = solution(xStart, yStart)
-        println(solution)
+    fun `GIVEN functions WHEN Newtons analytic derivative method is applied THEN solution is returned`() {
+        xesStart.zip(ysStart).forEachIndexed{ index, (xStart, yStart) ->
+            val solution = solution(xStart, yStart)
+            println(solution)
 
-        assertThat(solution.x).isBetween(xResultInterval.min(), xResultInterval.max())
-        assertThat(solution.y).isBetween(yResultInterval.min(), yResultInterval.max())
+            assertThat(solution.x).isBetween(xesResultIntervals[index].min(), xesResultIntervals[index].max())
+            assertThat(solution.y).isBetween(ysResultInterval[index].min(), ysResultInterval[index].max())
+        }
     }
 
     @Test
-    fun `GIVEN functions WHEN Newtons partial derivative method is applied THEN solution is returned`() {
-        val solution = solution(xStart, yStart, true)
-        println(solution)
+    fun `GIVEN functions WHEN Newtons numeric derivative method is applied THEN solution is returned`() {
+        xesStart.zip(ysStart).forEachIndexed{ index, (xStart, yStart) ->
+            val solution = solution(xStart, yStart, true)
+            println(solution)
 
-        assertThat(solution.x).isBetween(xResultInterval.min(), xResultInterval.max())
-        assertThat(solution.y).isBetween(yResultInterval.min(), yResultInterval.max())
-    }
-}
+            assertThat(solution.x).isBetween(xesResultIntervals[index].min(), xesResultIntervals[index].max())
+            assertThat(solution.y).isBetween(ysResultInterval[index].min(), ysResultInterval[index].max())
+        }
+    }}
